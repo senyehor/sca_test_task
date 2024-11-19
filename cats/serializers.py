0789a8d6b7b3aba4b0bd3034fb8fba94d216cec3
Yehor Sencha <senyehor@gmail.com>
2024-11-19 16:58:44 +0200

    feat: create spy cat serializer

---
+++ cats/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from cats.models import SpyCat
+
+
+class SpyCatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SpyCat
+        fields = ('id', 'name', 'years_of_experience', 'breed', 'salary')
+        read_only_fields = ('id',)
