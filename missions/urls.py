ed72c53fa89bd193ad42e9a1fe1406a059389fff
Yehor Sencha <senyehor@gmail.com>
2024-11-20 16:25:04 +0200

    feat: add url to update mission data and delete missions

--- missions/urls.py
+++ missions/urls.py
@@ -4,4 +4,8 @@ from missions.views import MissionViewSet
 
 urlpatterns = [
     path('missions/', MissionViewSet.as_view({'post': 'create', 'get': 'list'})),
+    path(
+        'missions/<int:pk>/',
+        MissionViewSet.as_view({'patch': 'partial_update', 'delete': 'delete'})
+    )
 ]
