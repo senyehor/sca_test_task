004dc69a17462d4a4eb9701b96ae2a3311e1848b
Yehor Sencha <senyehor@gmail.com>
2024-11-20 18:44:23 +0200

    feat: add nested router to update mission's targets

--- missions/urls.py
+++ missions/urls.py
@@ -1,12 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
+from rest_framework_nested.routers import NestedSimpleRouter
 
-from missions.views import MissionViewSet
+from missions.views import MissionViewSet, TargetUpdateView
 
 router = SimpleRouter()
 
 router.register('missions', MissionViewSet)
 
+missions_router = NestedSimpleRouter(router, r'missions', lookup='mission')
+missions_router.register(r'targets', TargetUpdateView, basename='mission-target')
+
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('', include(missions_router.urls))
 ]
