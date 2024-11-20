a298d8c55d994b6c1b7ebc03b6211b70d29cf943
Yehor Sencha <senyehor@gmail.com>
2024-11-20 20:22:40 +0200

    feat: add validating breed

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from cats.models import SpyCat
+from utils.cats_breeds import get_cats_breeds
 
 
 class SpyCatSerializer(serializers.ModelSerializer):
     class Meta:
         model = SpyCat
         fields = ('id', 'name', 'years_of_experience', 'breed', 'salary')
+
+    def validate_breed(self, value: str):
+        breeds = get_cats_breeds()
+        if value not in breeds:
+            raise ValidationError('Incorrect breed')
+        return value
