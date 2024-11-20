2cc7a152d17da11e1f25057459cd4cc9d3488dda
Yehor Sencha <senyehor@gmail.com>
2024-11-20 18:18:09 +0200

    ref: use special decorator to restrict PUT method

--- missions/views.py
+++ missions/views.py
@@ -1,11 +1,14 @@
 from django.core.exceptions import ValidationError as DjangoValidationError
 from django.db.transaction import atomic
 from django.utils.decorators import method_decorator
-from rest_framework.exceptions import MethodNotAllowed, ValidationError as DRFValidationError
+from rest_framework.exceptions import (
+    ValidationError as DRFValidationError,
+)
 from rest_framework.viewsets import ModelViewSet
 
 from missions.models import Mission
 from missions.serializers import MissionSerializer
+from utils.restrict_methods_for_view_set_method import restrict_methods
 
 
 class MissionViewSet(ModelViewSet):
@@ -19,10 +22,6 @@ class MissionViewSet(ModelViewSet):
         except DjangoValidationError as e:
             raise DRFValidationError(e.message)
 
+    @restrict_methods(('PUT',), message='Mission should be updated using PATCH')
     def update(self, request, *args, **kwargs):
-        if request.method == 'PUT':
-            # DRF considers PUT to have all model's fields and throws an exception
-            # when only some of them are included,
-            # which is the desired behavior of updating a mission
-            raise MethodNotAllowed('PUT', detail='Mission should be updated using PATCH')
         return super().update(request, *args, **kwargs)
