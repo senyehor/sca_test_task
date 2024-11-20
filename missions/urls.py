eab549e4a7e5110c4f41ed2ad1ab00de205a397f
Yehor Sencha <senyehor@gmail.com>
2024-11-20 20:06:01 +0200

    feat: add targets router to include notes' urls

--- missions/urls.py
+++ missions/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 from rest_framework_nested.routers import NestedSimpleRouter
 
-from missions.views import MissionViewSet, TargetUpdateView
+from missions.views import MissionViewSet, TargetNoteViewSet, TargetUpdateView
 
 router = SimpleRouter()
 
@@ -11,7 +11,11 @@ router.register('missions', MissionViewSet)
 missions_router = NestedSimpleRouter(router, r'missions', lookup='mission')
 missions_router.register(r'targets', TargetUpdateView, basename='mission-target')
 
+targets_router = NestedSimpleRouter(missions_router, 'targets', lookup='target')
+targets_router.register('notes', TargetNoteViewSet, basename='target-notes')
+
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include(missions_router.urls))
+    path('', include(missions_router.urls)),
+    path('', include(targets_router.urls))
 ]
