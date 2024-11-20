fd0894475bb0373e5270aecedd5565f76599bbc5
Yehor Sencha <senyehor@gmail.com>
2024-11-20 18:43:57 +0200

    feat: create view to update Target

--- missions/views.py
+++ missions/views.py
@@ -1,13 +1,15 @@
 from django.core.exceptions import ValidationError as DjangoValidationError
 from django.db.transaction import atomic
+from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from rest_framework.exceptions import (
-    ValidationError as DRFValidationError,
+    NotFound, ValidationError as DRFValidationError,
 )
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import UpdateModelMixin
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
-from missions.models import Mission
-from missions.serializers import MissionSerializer
+from missions.models import Mission, Target
+from missions.serializers import MissionSerializer, TargetSerializer
 from utils.restrict_methods_for_view_set_method import restrict_methods
 
 
@@ -23,5 +25,19 @@ class MissionViewSet(ModelViewSet):
             raise DRFValidationError(e.message)
 
     @restrict_methods(('PUT',), message='Mission should be updated using PATCH')
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+
+class TargetUpdateView(UpdateModelMixin, GenericViewSet):
+    serializer_class = TargetSerializer
+
+    def get_object(self):
+        mission_pk = self.kwargs['mission_pk']
+        if not Mission.objects.filter(id=mission_pk).exists():
+            raise NotFound(f'There is no mission with id {mission_pk}')
+        return get_object_or_404(Target, id=self.kwargs['pk'], mission_id=mission_pk)
+
+    @restrict_methods(('PUT',), message='Target should be updated using PATCH')
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
