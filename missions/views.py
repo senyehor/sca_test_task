b86ca7e773f2305b84a35906a71c55e51768436f
Yehor Sencha <senyehor@gmail.com>
2024-11-20 16:40:51 +0200

    feat: add converting django validation error to DRF validation error on delete

--- missions/views.py
+++ missions/views.py
@@ -1,3 +1,7 @@
+from django.core.exceptions import ValidationError as DjangoValidationError
+from django.db.transaction import atomic
+from django.utils.decorators import method_decorator
+from rest_framework.exceptions import ValidationError as DRFValidationError
 from rest_framework.viewsets import ModelViewSet
 
 from missions.models import Mission
@@ -7,3 +11,10 @@ from missions.serializers import MissionSerializer
 class MissionViewSet(ModelViewSet):
     queryset = Mission.objects.all()
     serializer_class = MissionSerializer
+
+    @method_decorator(atomic)
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super().destroy(request, *args, **kwargs)
+        except DjangoValidationError as e:
+            raise DRFValidationError(e.message)
