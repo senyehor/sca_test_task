42bcdd249f63a15f84ace37fcc4a672a61edc6a7
Yehor Sencha <senyehor@gmail.com>
2024-11-20 16:03:47 +0200

    feat: add GET missions list

--- missions/urls.py
+++ missions/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from missions.views import MissionViewSet
 
 urlpatterns = [
-    path('missions/', MissionViewSet.as_view({'post': 'create'}))
+    path('missions/', MissionViewSet.as_view({'post': 'create', 'get': 'list'})),
 ]
