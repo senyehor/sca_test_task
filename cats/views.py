b0b77e7977c5350bd434ea190d326bb5e76742e7
Yehor Sencha <senyehor@gmail.com>
2024-11-20 20:32:05 +0200

    feat: wrap cat deleting error into a DRF exception

--- cats/views.py
+++ cats/views.py
@@ -1,4 +1,8 @@
+from django.db import IntegrityError
+from django.db.transaction import atomic
+from django.utils.decorators import method_decorator
 from rest_framework import generics
+from rest_framework.exceptions import ValidationError
 
 from .models import SpyCat
 from .serializers import SpyCatSerializer
@@ -12,3 +16,13 @@ class SpyCatListCreateView(generics.ListCreateAPIView):
 class SpyCatDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = SpyCat.objects.all()
     serializer_class = SpyCatSerializer
+
+    @method_decorator(atomic)
+    def delete(self, request, *args, **kwargs):
+        try:
+            return super().delete(request, *args, **kwargs)
+        except IntegrityError as e:
+            raise ValidationError(
+                'Failed to delete cat due to db integrity error, '
+                'there probably are some dependent objects'
+            ) from e
