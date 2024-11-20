6c4ea2c349dbf0886e3b91ae95564230c41e92a2
Yehor Sencha <senyehor@gmail.com>
2024-11-20 20:23:52 +0200

    feat: add default DRF paginator

--- sca/settings.py
+++ sca/settings.py
@@ -37,6 +37,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':                7,
+}
+
 ROOT_URLCONF = 'sca.urls'
 
 TEMPLATES = [
